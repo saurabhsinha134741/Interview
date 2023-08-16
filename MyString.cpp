0226fafd1a7c8fdd7b55215f1d27c048b4304808
Saurabh Sinha <saurabh134741@gmail.com>
2023-08-16 08:35:42 +0530

    Update MyString.cpp
    Added crore with one argument to custom string class

--- MyString.cpp
+++ MyString.cpp
@@ -1,3 +1,5 @@
+#include <cstring>
+
 class MyString{
 char *str;
 unsigned int len; // len >= 0
@@ -5,4 +7,21 @@ public:
 //default constructor 
 MyString(): str(NULL), len(0) //initializer list
 {}
+MyString(char *s); //ctor with one argument
 };
+
+//ctor with one argument
+MyString::MyString(char *s){
+  if(s == NULL){
+    str=new char[1];
+    str[0]='\0';
+  }
+  len=strlen(s);
+  str=new char[len+1];
+  strcpy(str, s);
+  str[len]='\0';
+}
+int main(){
+  MyString s1("Hello");
+  return 0;
+}
